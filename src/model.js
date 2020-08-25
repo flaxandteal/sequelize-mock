934626f04db28c178ed16a62d35bfd8a54d7d65f
Phil Weir <phil.weir@flaxandteal.co.uk>
2020-08-25 14:31:28 +0100

    object method to remove an automatic attribute

--- src/model.js
+++ src/model.js
@@ -271,6 +271,14 @@ fakeModel.prototype.scope = function () {
 fakeModel.prototype.addScope = function () {};
 
 /**
+ * No-op that returns a void.
+ * 
+ * @instance
+ * @return {undefined}
+ **/
+fakeModel.prototype.removeAttribute = function () {};
+
+/**
  * Executes a mock query to find all of the instances with any provided options. Without
  * any other configuration, the default behavior when no queueud query result is present
  * is to create an array of a single result based on the where query in the options and
